5405b85db1181c406ae22c4cb4c8d9c7b3ddd776
Giedrius Stabužis <giedrius.stabuzis@gmail.com>
2022-11-04 18:15:18 +0200

    Pridetas modulis calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,8 @@
+from utils import helpers
+
+class Calculator:
+    def __init__(self) -> None:
+        helpers.health()
+
+if __name__ == "__main__":
+    c=Calculator()
---
+++ calculator/utils/helpers.py
@@ -0,0 +1,5 @@
+from calculator.calculator import Calculator
+
+def health() -> None:
+    return Calculator ()
+    #print("code is working")
