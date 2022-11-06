ceef202511dc3dac1b73997773e5971971941348
Giedrius Stabužis <giedrius.stabuzis@gmail.com>
2022-11-06 15:58:47 +0200

    pre-comit

--- src/calculator.py
+++ src/calculator.py
@@ -1,14 +1,20 @@
 from utils import helpers
 
 
+
+
 class Calculator:
     def __init__(self) -> None:
         helpers.health()
         print("labas")
 
 
+
+
 def sum(a: int, b: int) -> int:
     return a + b
+    
+
 
 
 if __name__ == "__main__":
